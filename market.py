82cdb0e1068a8fa27c5ecd1e4858912e8cedcbbb
Shubham Kadariya <shubham.kadariya021@apexcollege.edu.np>
2024-11-28 19:03:57 +0545

    Jinja Templating

--- market.py
+++ market.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def helloWorld():
-    return '<p> Hello World!</p>'
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
 
-@app.route('/about/<username>')
-def aboutPage(username):
-    return f'<h1> This is a about page for {username}</h1>'
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup_page():
+    return render_template('signup.html')
+
+@app.route('/market')
+def market_page():
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+    return render_template('market.html', items=items)
+
+# @app.route('/about/<username>')
+# def about_page(username):
+#     return f'<h1> This is a about page for {username}</h1>'
